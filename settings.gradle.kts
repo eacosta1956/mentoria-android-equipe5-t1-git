d70db6f890c48782cb42f7b1a93ca23b77fbf131
eacosta1956 <eldon.costa@terra.com.br>
2025-07-04 11:56:49 -0300

    git commit -m "Commit inicial da estrutura do projeto"

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Mentoria_Android_T1_E5_Git"
+include(":app")
