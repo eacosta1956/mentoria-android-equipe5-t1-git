6e196b025819b7a1eeef3bc85446f91926699025
JeanAlencar <jeanalencar@hotmail.com>
2025-07-07 19:22:48 -0300

    feat adiciona esqueleto para DetalhesRestauranteActivity

---
+++ app/src/main/java/com/example/mentoria_android_t1_e5_git/DetalhesRestauranteActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mentoria_android_t1_e5_git
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DetalhesRestauranteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhes_restaurante)
+    }
+}
