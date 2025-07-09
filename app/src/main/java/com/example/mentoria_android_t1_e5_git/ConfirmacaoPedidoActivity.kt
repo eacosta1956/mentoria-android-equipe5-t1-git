ace03c325394c2eeff13db0888b66d0161aadff4
eacosta1956 <eldon.costa@terra.com.br>
2025-07-09 13:55:23 -0300

    commit da feature/esqueleto_confirmacao_pedido

---
+++ app/src/main/java/com/example/mentoria_android_t1_e5_git/ConfirmacaoPedidoActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mentoria_android_t1_e5_git
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ConfirmacaoPedidoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirmacao_pedido)
+    }
+}
